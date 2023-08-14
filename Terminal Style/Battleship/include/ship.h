e11e3f63a259a48f21e871c90ec20f9cb8f3271e
Simone Finelli <simonefinelli.work@gmail.com>
2023-08-14 16:29:45 +0200

    Add ship.cpp and ship.h

---
+++ Terminal Style/Battleship/include/ship.h
@@ -0,0 +1,52 @@
+/**
+ * @file ship.h
+ *
+ * @author SimoX
+ * @date 2023-08-14
+ */
+
+#ifndef BATTLESHIP_SHIP_H
+#define BATTLESHIP_SHIP_H
+
+#define SHIPS_NUMBER 5
+#define AIRCRAFT_CARRIER_LENGTH 5
+#define BATTLESHIP_LENGTH 4
+#define CRUISER_LENGTH 3
+#define SUBMARINE_LENGTH 3
+#define DESTROYER_LENGTH 2
+
+typedef enum ShipType {
+    NO_TYPE = 0,
+    AIRCRAFT_CARRIER,
+    BATTLESHIP,
+    CRUISER,
+    SUBMARINE,
+    DESTROYER
+} ship_type;
+
+typedef enum ShipOrientation {
+    NO_ORIENTATION,
+    HORIZONTAL = 0,
+    VERTICAL
+} ship_orientation;
+
+typedef struct ShipCoordinates {
+    int x; // rows (A-J)
+    int y; // cols (1-10)
+} ship_coordinates;
+
+typedef enum ShipStatus {
+    SUNK=0,
+    ALIVE
+} ship_status;
+
+struct Ship {
+    ship_type type = NO_TYPE;
+    int length = 0;
+    ship_orientation orientation = NO_ORIENTATION;
+    ship_coordinates coordinates = {};
+    ship_status status=ALIVE;
+};
+
+
+#endif //BATTLESHIP_SHIP_H
---
+++ Terminal Style/Battleship/src/ship.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file ship.cpp
+ * @brief
+ *
+ * @author SimoX
+ * @date 2023-08-14
+ */
+
+#include "ship.h"
