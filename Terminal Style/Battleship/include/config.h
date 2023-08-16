48bae11e804f7719d304224fae93bb05f495e676
Simone Finelli <simonefinelli.work@gmail.com>
2023-08-16 10:46:04 +0200

    Add config.h

---
+++ Terminal Style/Battleship/include/config.h
@@ -0,0 +1,15 @@
+/**
+ * @file config.h
+ *
+ * @author SimoX
+ * @date 2023-08-14
+ */
+
+#ifndef BATTLESHIP_CONFIG_H
+#define BATTLESHIP_CONFIG_H
+
+struct GameData {
+
+};
+
+#endif //BATTLESHIP_CONFIG_H
