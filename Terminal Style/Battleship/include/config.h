770460a525a9e578eb2b04ac32ef91c686e59582
Simone Finelli <simonefinelli.work@gmail.com>
2023-08-17 18:20:50 +0200

    Define game data structure

--- Terminal Style/Battleship/include/config.h
+++ Terminal Style/Battleship/include/config.h
@@ -5,11 +5,14 @@
  * @date 2023-08-14
  */
 
+#include "player.h"
+
 #ifndef BATTLESHIP_CONFIG_H
 #define BATTLESHIP_CONFIG_H
 
 struct GameData {
-
+    Player player1 {};
+    Player player2 {};
 };
 
 #endif //BATTLESHIP_CONFIG_H
