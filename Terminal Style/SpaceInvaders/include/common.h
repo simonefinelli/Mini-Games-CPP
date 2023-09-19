d9249c7b59ce6d4b2d9d5cd98b27c0245b472c15
Simone Finelli <simonefinelli.work@gmail.com>
2023-09-19 17:26:56 +0200

    Add common structures and constants

--- Terminal Style/SpaceInvaders/include/common.h
+++ Terminal Style/SpaceInvaders/include/common.h
@@ -1,13 +1,19 @@
 /**
  * @file battlefield.h
  * @brief This file contains common structures shared between different game
- * objects.
+ * objects and files.
  * @author SimoX
  * @date 2023-09-09
  */
 
 #ifndef SPACEINVADERS_COMMON_H
 #define SPACEINVADERS_COMMON_H
+
+// playing field
+#define W_WIDTH 68 // columns
+#define W_HEIGHT 100 // rows
+
+#define SPRITE_HEIGHT 2
 
 /**
  * @brief Game coordinates are calculated starting form 0.
@@ -17,6 +23,9 @@ typedef struct GameCoordinates {
     int y = -1;  // rows
 } coords;
 
+/**
+ * @brief Game objects sizes are define ad width and height units.
+ */
 typedef struct ObjectSize {
     int width = 0;
     int height = 0;
