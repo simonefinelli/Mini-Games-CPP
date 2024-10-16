be04a062dd8bd8f0ee2f2f94128f45ac624dd5eb
Simone Finelli <simonefinelli.work@gmail.com>
2024-10-16 11:41:17 +0200

    Refactor: Vec2D class

--- Arcade/Breakout/utils/Vec2D.cpp
+++ Arcade/Breakout/utils/Vec2D.cpp
@@ -1,1 +1,16 @@
 #include "Vec2D.h"
+
+// static members - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+const Vec2D Vec2D::Zero;
+
+// constructors - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// operator overloading - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::ostream& operator<<(std::ostream& out, const Vec2D& vec) {
+    out << "Vec(x,y): (" << vec.x_coord << "," << vec.y_coord << ")";
+    return out;
+}
+
+// public interface - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// private interface - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- Arcade/Breakout/utils/Vec2D.h
+++ Arcade/Breakout/utils/Vec2D.h
@@ -4,31 +4,22 @@
 #include <iostream>
 
 class Vec2D {
-
 public:
+    // static members
     static const Vec2D Zero;
-
+    // constructors
     Vec2D() : Vec2D(0, 0) {}
     Vec2D(float x, float y) : x_coord(x), y_coord(y) {}
-
+    // methods
     inline void set_x(float x) { x_coord = x; }
     inline void set_y(float y) { y_coord = y; }
     inline float get_x() const { return x_coord; }
     inline float get_y() const { return y_coord; }
-
     // operator overloading
     friend std::ostream& operator<<(std::ostream& out, const Vec2D& vec);
-
 private:
     float x_coord;
     float y_coord;
 };
-
-const Vec2D Vec2D::Zero;
-
-std::ostream& operator<<(std::ostream& out, const Vec2D& vec) {
-    out << "Vec(x,y): (" << vec.x_coord <<"," << vec.y_coord << ")";
-    return out;
-}
 
 #endif // VEC2D_LIB_H
