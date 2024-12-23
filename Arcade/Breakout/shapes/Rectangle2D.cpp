e6e47e45ada8feaebb57d8dee220249839af1582
Simone Finelli <simonefinelli.work@gmail.com>
2024-12-23 17:00:07 +0100

    Chore: continue Rectangle2D implementation

--- Arcade/Breakout/shapes/Rectangle2D.cpp
+++ Arcade/Breakout/shapes/Rectangle2D.cpp
@@ -5,4 +5,36 @@
  * @date 2024-12-22
  */
 
+#include <cmath>
 #include "Rectangle2D.h"
+
+
+// ========================================================================== //
+// Public interface                                                           //
+// ========================================================================== //
+
+// Constructors ============================================================= //
+Rectangle2D::Rectangle2D() : Rectangle2D(Vec2D::ZERO, Vec2D::ZERO) {}
+
+Rectangle2D::Rectangle2D(const Vec2D& top_left, unsigned int width, unsigned int height) {
+    Vec2D bottom_right(top_left.get_x() + width - 1, 
+                       top_left.get_y() + height - 1);
+    
+    m_points.push_back(top_left);
+    m_points.push_back(bottom_right);
+}
+
+Rectangle2D::Rectangle2D(const Vec2D& top_left, const Vec2D& bottom_right) {
+    m_points.push_back(top_left);
+    m_points.push_back(bottom_right);
+}
+
+// Instance methods ========================================================= //
+
+float get_width() const {
+
+}
+
+float get_height() const {
+    
+}
