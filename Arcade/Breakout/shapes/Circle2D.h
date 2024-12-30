d0920c8e62899b0f8278b662de457cfcba1e9255
Simone Finelli <simonefinelli.work@gmail.com>
2024-12-30 15:27:50 +0100

    Chore: finalize Circle2D class implementation

--- Arcade/Breakout/shapes/Circle2D.h
+++ Arcade/Breakout/shapes/Circle2D.h
@@ -13,18 +13,22 @@
 
 class Circle2D : public Shape2D {
 public:
+    // Constructors ========================================================= //     
     Circle2D();
     Circle2D(const Vec2D& center, float radius);
 
+    // Instance methods ===================================================== //
     inline virtual Vec2D get_center_point() const override { return m_points[0]; }
     inline float get_radius () { return m_radius; }
     inline void set_radius(float radius) { m_radius = radius; }
     inline void move_to(const Vec2D& position) { m_points[0] = position; }
 
-    bool intersects(const Circle2D& otherCircle) const;
+    bool intersects(const Circle2D& other_circle) const;
     bool contains_point(const Vec2D& point) const;
 
+    // Operator overloading ================================================= //
 private:
+    // Instance variables =================================================== //
     float m_radius;
 };
 
