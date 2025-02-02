a877732e643d436cf0d4e1bd1f3d3290c1cd2576
Simone Finelli <simonefinelli.work@gmail.com>
2025-02-02 16:26:05 +0100

    Refactor: use scientific notation for epsilon error value

--- Arcade/libs/Vec2D/include/vec2D_utils.h
+++ Arcade/libs/Vec2D/include/vec2D_utils.h
@@ -1,7 +1,7 @@
 #ifndef VEC2D_UTILS_H
 #define VEC2D_UTILS_H
 
-static const float EPSILON = 0.0001f;  // tolerance for floating-point calculations
+static const float EPSILON = 1e-4f;  // tolerance for floating-point calculations
 
 bool is_equal(float x, float y);
 bool is_greaten_than_or_equal(float x, float y);
